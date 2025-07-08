0ec5a526fc447238175578eee71a6d844f51f8f6
cybers232024 <cybers232024@gmail.com>
2025-07-08 12:55:54 +0000

    html & jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.jsx';
+import Dd from 'd.html';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Dd/>
   </StrictMode>,
 )
